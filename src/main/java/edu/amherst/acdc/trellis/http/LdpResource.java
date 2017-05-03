d75bd3adb288f605141bac5b828d68c03815250a
Aaron Coburn <acoburn@amherst.edu>
2017-05-03 13:52:38 -0400

    Refactor Memento-related code

--- src/main/java/edu/amherst/acdc/trellis/http/LdpResource.java
+++ src/main/java/edu/amherst/acdc/trellis/http/LdpResource.java
@@ -15,37 +15,31 @@
  */
 package edu.amherst.acdc.trellis.http;
 
-import static edu.amherst.acdc.trellis.http.Constants.APPLICATION_LINK_FORMAT;
 import static edu.amherst.acdc.trellis.http.Constants.ACCEPT_DATETIME;
 import static edu.amherst.acdc.trellis.http.Constants.ACCEPT_PATCH;
 import static edu.amherst.acdc.trellis.http.Constants.ACCEPT_POST;
+import static edu.amherst.acdc.trellis.http.Constants.APPLICATION_LINK_FORMAT;
 import static edu.amherst.acdc.trellis.http.Constants.MEMENTO_DATETIME;
 import static edu.amherst.acdc.trellis.http.Constants.PREFER;
 import static edu.amherst.acdc.trellis.http.Constants.PREFERENCE_APPLIED;
 import static edu.amherst.acdc.trellis.http.Constants.TRELLIS_PREFIX;
 import static edu.amherst.acdc.trellis.http.Constants.VARY;
-import static edu.amherst.acdc.trellis.http.Constants.WANT_DIGEST;
+//import static edu.amherst.acdc.trellis.http.Constants.WANT_DIGEST;
 import static edu.amherst.acdc.trellis.http.RdfMediaType.APPLICATION_LD_JSON;
 import static edu.amherst.acdc.trellis.http.RdfMediaType.APPLICATION_N_TRIPLES;
 import static edu.amherst.acdc.trellis.http.RdfMediaType.APPLICATION_SPARQL_UPDATE;
 import static edu.amherst.acdc.trellis.http.RdfMediaType.TEXT_TURTLE;
 import static edu.amherst.acdc.trellis.http.RdfMediaType.VARIANTS;
 import static edu.amherst.acdc.trellis.spi.ConstraintService.ldpResourceTypes;
-import static java.time.ZonedDateTime.ofInstant;
-import static java.time.ZonedDateTime.parse;
-import static java.time.ZoneOffset.UTC;
-import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
 import static java.util.Date.from;
 import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
-import static java.util.stream.Stream.concat;
 import static java.util.stream.Stream.empty;
 import static java.util.stream.Stream.of;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.APPLICATION_OCTET_STREAM;
 import static javax.ws.rs.core.MediaType.TEXT_HTML;
-import static javax.ws.rs.core.Response.Status.FOUND;
 import static javax.ws.rs.core.Response.Status.GONE;
 import static javax.ws.rs.core.Response.Status.NOT_ACCEPTABLE;
 import static javax.ws.rs.core.Response.Status.NOT_FOUND;
@@ -59,21 +53,16 @@ import com.codahale.metrics.annotation.Timed;
 
 import edu.amherst.acdc.trellis.api.Datastream;
 import edu.amherst.acdc.trellis.api.Resource;
-import edu.amherst.acdc.trellis.api.VersionRange;
 import edu.amherst.acdc.trellis.spi.DatastreamService;
 import edu.amherst.acdc.trellis.spi.NamespaceService;
 import edu.amherst.acdc.trellis.spi.ResourceService;
 import edu.amherst.acdc.trellis.spi.SerializationService;
 import edu.amherst.acdc.trellis.vocabulary.LDP;
 import edu.amherst.acdc.trellis.vocabulary.OA;
-import edu.amherst.acdc.trellis.vocabulary.PROV;
 import edu.amherst.acdc.trellis.vocabulary.Trellis;
-import edu.amherst.acdc.trellis.vocabulary.XSD;
 
 import java.io.InputStream;
 import java.time.Instant;
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -165,80 +154,35 @@ public class LdpResource {
         final String urlPrefix = ofNullable(baseUrl).orElseGet(() -> uriInfo.getBaseUri().toString());
         final String identifier = urlPrefix + path;
         final Optional<RDFSyntax> syntax = getRdfSyntax(headers.getAcceptableMediaTypes());
-        final Optional<Instant> acceptDatetime = ofNullable(headers.getRequestHeaders().getFirst(ACCEPT_DATETIME))
-                .map(x -> parse(x, RFC_1123_DATE_TIME)).map(ZonedDateTime::toInstant);
-        final Optional<Instant> memento = ofNullable(uriInfo.getQueryParameters().getFirst("memento"))
-                .map(String::trim).map(Long::parseLong).map(Instant::ofEpochMilli);
-        final Optional<String> wantDigest = ofNullable(headers.getRequestHeaders().getFirst(WANT_DIGEST));
-        final Boolean timemap = ofNullable(uriInfo.getQueryParameters().getFirst("timemap"))
-                .map(String::trim).map(Boolean::new).orElse(false);
-
-        LOGGER.info("RDF Syntax: {}", syntax.map(RDFSyntax::toString).orElse("Nothing"));
+        final Optional<Instant> acceptDatetime = MementoResource.getAcceptDatetime(headers);
+        final Optional<Instant> version = MementoResource.getVersionParam(uriInfo);
+        final Boolean timemap = MementoResource.getTimeMapParam(uriInfo);
+        // TODO -- add digest support
+        //final Optional<String> wantDigest = ofNullable(headers.getRequestHeaders().getFirst(WANT_DIGEST));
 
         final Optional<Resource> resource;
-        if (memento.isPresent()) {
-            resource = resourceService.get(rdf.createIRI(TRELLIS_PREFIX + path), memento.get());
+        if (version.isPresent()) {
+            LOGGER.info("Getting versioned resource: {}", version.get().toString());
+            resource = resourceService.get(rdf.createIRI(TRELLIS_PREFIX + path), version.get());
 
         } else if (timemap) {
-            return resourceService.get(rdf.createIRI(TRELLIS_PREFIX + path)).map(res -> {
-                final Response.ResponseBuilder builder = Response.ok().link(identifier, "original timegate");
-                final List<VersionRange> ranges = res.getMementos().collect(toList());
-                final Stream<Link> links = concat(ranges.stream()
-                        .reduce((acc, x) -> new VersionRange(acc.getFrom(), x.getUntil()))
-                        .map(x -> Link.fromUri(identifier + "?timemap=true").rel("self").type(APPLICATION_LINK_FORMAT)
-                                .param("from", ofInstant(x.getFrom(), UTC).format(RFC_1123_DATE_TIME))
-                                .param("until", ofInstant(x.getUntil(), UTC).format(RFC_1123_DATE_TIME)).build())
-                        .map(Stream::of).orElse(empty()),
-                    ranges.stream().map(range ->
-                        Link.fromUri(identifier + "?memento=" + range.getFrom().toEpochMilli()).rel("memento")
-                            .param("datetime", ofInstant(range.getFrom(), UTC).format(RFC_1123_DATE_TIME))
-                            .build()));
-
-                if (syntax.isPresent()) {
-                    final Stream<Quad> quads = links.flatMap(link -> {
-                        final IRI iri = rdf.createIRI(link.getUri().toString());
-                        final List<Quad> buffer = new ArrayList<>();
-                        if (link.getParams().containsKey("from")) {
-                            buffer.add(rdf.createQuad(Trellis.PreferUserManaged, iri, PROV.startedAtTime,
-                                        rdf.createLiteral(parse(link.getParams().get("from"),
-                                                RFC_1123_DATE_TIME).toString(), XSD.dateTime)));
-                        }
-                        if (link.getParams().containsKey("until")) {
-                            buffer.add(rdf.createQuad(Trellis.PreferUserManaged, iri, PROV.endedAtTime,
-                                        rdf.createLiteral(parse(link.getParams().get("until"),
-                                                RFC_1123_DATE_TIME).toString(), XSD.dateTime)));
-                        }
-                        if ("memento".equals(link.getParams().get("rel")) && link.getParams().containsKey("datetime")) {
-                            buffer.add(rdf.createQuad(Trellis.PreferUserManaged, iri, PROV.atTime,
-                                        rdf.createLiteral(parse(link.getParams().get("datetime"),
-                                                RFC_1123_DATE_TIME).toString(), XSD.dateTime)));
-                        }
-                        return buffer.stream();
-                    });
-                    builder.entity(new ResourceStreamer(serializationService, quads, syntax.get()));
-                } else {
-                    builder.entity(links.map(Link::toString).collect(joining(",\n")) + "\n");
-                }
-                return builder;
-            }).orElse(Response.status(NOT_FOUND)).build();
+            return resourceService.get(rdf.createIRI(TRELLIS_PREFIX + path)).map(MementoResource::new)
+                .map(res -> res.getTimeMapBuilder(identifier, syntax, serializationService))
+                .orElse(Response.status(NOT_FOUND)).build();
 
         } else if (acceptDatetime.isPresent()) {
-            return resourceService.get(rdf.createIRI(TRELLIS_PREFIX + path), acceptDatetime.get()).map(res ->
-                Response.status(FOUND)
-                    .location(fromUri(identifier + "?memento=" + acceptDatetime.map(Instant::toEpochMilli).get())
-                        .build())
-                    .link(identifier, "original timegate")
-                    .links(Link.fromUri(identifier + "?timemap=true").rel("timemap").type(APPLICATION_LINK_FORMAT)
-                        .build())
-                    .header(VARY, ACCEPT_DATETIME)).orElse(Response.status(NOT_FOUND)).build();
+            return resourceService.get(rdf.createIRI(TRELLIS_PREFIX + path), acceptDatetime.get())
+                .map(MementoResource::new).map(res -> res.getTimeGateBuilder(identifier, acceptDatetime.get()))
+                .orElse(Response.status(NOT_FOUND)).build();
+
         } else {
             resource = resourceService.get(rdf.createIRI(TRELLIS_PREFIX + path));
         }
 
         return resource.map(res -> {
             if (res.getTypes().anyMatch(Trellis.DeletedResource::equals)) {
-                // TODO add Mementos
-                return Response.status(GONE);
+                return Response.status(GONE)
+                    .links(MementoResource.getMementoLinks(identifier, res.getMementos()).toArray(Link[]::new));
             }
 
             final Response.ResponseBuilder builder = Response.ok();
@@ -282,14 +226,14 @@ public class LdpResource {
             res.getAnnotationService().map(IRI::getIRIString).ifPresent(svc ->
                     builder.link(svc, OA.annotationService.getIRIString()));
 
+            // Memento-related headers
             if (res.isMemento()) {
                 builder.header(MEMENTO_DATETIME, from(res.getModified()));
             } else {
                 builder.header(VARY, ACCEPT_DATETIME);
             }
             builder.link(identifier, "original timegate");
-            builder.links(Link.fromUri(identifier + "?timemap=true").rel("timemap").type(APPLICATION_LINK_FORMAT)
-                        .build());
+            builder.links(MementoResource.getMementoLinks(identifier, res.getMementos()).toArray(Link[]::new));
 
             // NonRDFSources responses (strong ETags, etc)
             if (res.getDatastream().isPresent() && !syntax.isPresent()) {
@@ -330,10 +274,6 @@ public class LdpResource {
             }
 
             // TODO add acl header, if in effect
-            // add Memento headers
-            // res.getMementos().forEach(range -> {
-            //     builder.header(...);
-            // });
             // TODO check cache control headers
             // TODO add support for instance digests
             // TODO add support for range requests
@@ -377,6 +317,7 @@ public class LdpResource {
     private static final Function<MediaType, Stream<RDFSyntax>> getSyntax = type -> {
         final Optional<RDFSyntax> syntax = VARIANTS.stream().map(Variant::getMediaType).filter(type::isCompatible)
             .findFirst().map(MediaType::toString).flatMap(RDFSyntax::byMediaType);
+        // TODO replace with Optional::stream with JDK 9
         return syntax.isPresent() ? of(syntax.get()) : empty();
     };
 
